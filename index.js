10d1045e984d98fb1aa64813d90f54f3b307dbf1
Graham Swan <thinkswan@gmail.com>
2019-07-04 12:41:49 +0200

    Add LinkedList class

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const n2 = {
 
 n1.next = n2
 
-console.log("n1:", n1) // n1: { data: 100, next: { data: 200 } }
+console.log("n1", n1) // n1: { data: 100, next: { data: 200 } }
 
 /*
  * Class implementation of a node
@@ -27,4 +27,28 @@ class Node {
 
 const n3 = new Node(100)
 
-console.log("n3:", n3) // n3: Node { data: 100, next: null }
+console.log("n3", n3) // n3: Node { data: 100, next: null }
+
+/*
+ * Class implementation of a linked list
+ */
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.size = 0
+  }
+
+  toString() {
+    let current = this.head
+
+    while (current) {
+      console.log("Node", {
+        data: current.data,
+        next: current.next ? current.constructor : current.next
+      })
+
+      current = current.next
+    }
+  }
+}
