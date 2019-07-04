eb1f58a88db6cf4fc15b980a69d5ef06ee6d8669
Graham Swan <thinkswan@gmail.com>
2019-07-04 11:36:56 +0200

    Add class implementation of a node

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+ * Basic example of a linked list
+ */
+
 const n1 = {
   data: 100
 }
@@ -9,3 +13,18 @@ const n2 = {
 n1.next = n2
 
 console.log("n1:", n1) // n1: { data: 100, next: { data: 200 } }
+
+/*
+ * Class implementation of a node
+ */
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data
+    this.next = next
+  }
+}
+
+const n3 = new Node(100)
+
+console.log("n3:", n3) // n3: Node { data: 100, next: null }
