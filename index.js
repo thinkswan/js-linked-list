19657672bc408d8ce87729a0c7f61e0f3c7b8b35
Graham Swan <thinkswan@gmail.com>
2019-07-04 12:43:10 +0200

    Remove WIP examples from code

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-/*
- * Basic example of a linked list
- */
-
-const n1 = {
-  data: 100
-}
-
-const n2 = {
-  data: 200
-}
-
-n1.next = n2
-
-console.log("n1", n1) // n1: { data: 100, next: { data: 200 } }
-
-/*
- * Class implementation of a node
- */
-
 class Node {
   constructor(data, next = null) {
     this.data = data
     this.next = next
   }
 }
-
-const n3 = new Node(100)
-
-console.log("n3", n3) // n3: Node { data: 100, next: null }
-
-/*
- * Class implementation of a linked list
- */
 
 class LinkedList {
   constructor() {
